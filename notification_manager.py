05e3522623e466dd4ec1ef9cbc572352f899b32d
Kabiru-254 <55836681+Kabiru-254@users.noreply.github.com>
2022-10-04 13:12:51 +0300

    Update notification_manager.py
    Retraction

--- notification_manager.py
+++ notification_manager.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 import requests
 
-API_KEY = "43acb4d7e845d481a06876384a2d0abe"
-twilio_account_sid = "AC84013a64fc4b82e966fa50d6266a41c1"
-auth_token = "9ba67f5874e000b9ef20da3dc42481b1"
+API_KEY = "Use your own API key"
+twilio_account_sid = "Use your account SID"
+auth_token = "Use your own Auth Token"
 
 
 class NotificationManager:
@@ -12,8 +12,8 @@ class NotificationManager:
 
     def send_message(self, details: dict):
         message = self.twilio_client.messages.create(
-            from_='+19897621146',
-            to='+254724802599',
+            from_='Your twilio number',
+            to='Recipent number',
             body=f"Low price Alert! Only €{details['price']} to fly from {details['origin_city']}-"
                  f"{details['origin_airport']} to {details['destination_city']}-{details['destination_airport']} from "
                  f"{details['out_date']} to {details['return_date']}!"
